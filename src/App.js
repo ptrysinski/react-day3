0ca087d8915c5f3024da3039c6056e59093be1e1
Piotr Trysiński <ptrysinski@gmail.com>
2019-04-03 14:55:11 +0200

    end of day 3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import Counter from './Counter/Counter';
 import TypeName from './TypeName/TypeName';
 import Lifecycle from './Lifecycle/Lifecycle';
 
 const App = (props) => (
-  <div>
-    <Counter />
-    <TypeName />
-    <Lifecycle />
-  </div>
+  
+    <Router>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Lifecycle">Lifecycle</Link>
+        </li>
+        <li>
+          <Link to="/Counter">Counter</Link>
+        </li>
+        <li>
+          <Link to="/TypeName">TypeName</Link>
+        </li>
+      </ul>
+
+      <Route path={'/Counter'} component={Counter} />
+      <Route path={'/TypeName'} component={TypeName} />
+      <Route path={'/Lifecycle'} component={Lifecycle} />
+    </Router>
+
 )
 
 
